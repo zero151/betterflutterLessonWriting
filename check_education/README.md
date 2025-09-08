9302ec52d9c81a2d0cf070abec7b1e1382bc7b52
Zero <Zero39@list.ru>
2025-09-08 21:22:14 +0300

    delete dont need folders

--- check_education/README.md
+++
@@ -1,3 +0,0 @@
-# check_education
-
-A new Flutter project.
--- check_education/android/app/src/main/kotlin/com/example/check_education/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.check_education
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
